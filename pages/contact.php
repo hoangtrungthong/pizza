8f6edc71c994521c2edc7728021faf5bdc523244
hoang trung thong <hoangtrungthong0000@gmail.com>
2021-09-03 16:46:41 +0700

    bot

--- pages/contact.php
+++ pages/contact.php
@@ -1,14 +1,3 @@
-<?php
-// get all products
-$sqlProducts = "SELECT * FROM products";
-
-
-$query = mysqli_query($conn, $sqlProducts);
-$products = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-// get size
-?>
-
 <section class="order" id="order">
     <h2><span class="highlight">Liên hệ </span>với chúng tôi</h2>
     <div class="row">
@@ -30,6 +19,5 @@ $products = mysqli_fetch_all($query, MYSQLI_ASSOC);
         <div class="image map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.1375347822477!2d105.77592391440801!3d21.067168391843023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134552c72eb9b5f%3A0x9c84e103e69e4c95!2zTmfDtSA0MDEgxJDGsOG7nW5nIEPhu5UgTmh14bq_LCBD4buVIE5odeG6vyAyLCBC4bqvYyBU4burIExpw6ptLCBIw6AgTuG7mWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1630410484865!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
-
     </div>
 </section>
